d33fbdfaad5845e87a8c95c47516e4484729eacc
pepap <jpasulka@gmail.com>
2023-11-06 14:03:35 +0100

    Update 01.mapping--create-index.bash

--- 01.mapping--create-index.bash
+++ 01.mapping--create-index.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exeCMD="/storage/brno1-cerit/home/pepap/pLIBRARY/STAR-2.7.0c/bin/Linux_x86_64_static/STAR"
-inpFA="/storage/brno3-cerit/home/pepap/Annotations/SNP-InDels-mice-strains/GRCm38_68.fa"
+exeCMD="STAR"
+inpFA="GRCm38_68.fa"
 
 ${exeCMD} --runMode          genomeGenerate \
           --runThreadN       16             \
